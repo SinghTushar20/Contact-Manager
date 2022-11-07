27f7218251ecdcd7c0950c062c99cc4f6e65fc8b
SinghTushar20 <103422336+SinghTushar20@users.noreply.github.com>
2022-11-07 21:07:38 +0530

    contact manager created

---
+++ headers/Phonebook.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+class Phonebook
+{
+    public:
+    vector<string> firstNames;
+    vector<string> lastNames;
+    vector<string> mobileNumber;
+
+    void add(string firstName, string lastName, string phone)
+    {
+        transform(firstName.begin(),firstName.end(),firstName.begin(),::tolower);
+        transform(lastName.begin(),lastName.end(),lastName.begin(),::tolower);
+        firstNames.push_back(firstName);
+        lastNames.push_back(lastName);
+        mobileNumber.push_back(phone);
+    }    
+};
